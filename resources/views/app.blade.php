5a70ef3eec9d5ec044c3beb47d37cb3f866ff347
Kent Vincent Malaran <kentvincentmalaran46@gmail.com>
2025-01-01 22:28:43 +0800

    Replace welcome view with app view and add new CSS styles

---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>30 days Laravel Roadmap</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+
+<body>
+
+    <h1>Hello, from welcome Page!</h1>
+
+
+
+</body>
+
+</html>
